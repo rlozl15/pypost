601a5cf5009a155eb3de7d9d614366646386cc5c
rlozl15 <eunjikim.dev@gmail.com>
2024-11-16 04:16:37 +0900

    feat: swagger 기본 셋팅

--- pypost/settings.py
+++ pypost/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'drf_yasg',
+    'rest-framework'
 ]
 
 MIDDLEWARE = [
